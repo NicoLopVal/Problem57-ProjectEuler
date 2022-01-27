f73ec0cf453d4a924763a62d012f407f1682e61c
nikil <nikil@DESKTOP-73MHGNO>
2022-01-26 21:21:43 -0500

    Add project files.

---
+++ Problem57-ProjectEuler/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+BigInteger numerator = 3;
+BigInteger denominator = 2;
+double answer = 0;
+
+for(int i = 1; i <= 1000; i++)
+{
+    if (numerator.ToString().Length > denominator.ToString().Length)
+        answer++;
+    BigInteger oldDenominator = denominator;
+    denominator = numerator + denominator;
+    numerator = oldDenominator + denominator;
+}
+
+Console.WriteLine("In the first one-thousand expansions, there are {0} fractions contain a " +
+    "numerator with more digits than the denominator",answer);
